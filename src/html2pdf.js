242e407bb6b3b57df45a96fd7f16d0a24bdd4d79
Brian Rutledge <bhrutledge@gmail.com>
2018-08-26 21:13:33 -0400

    Add dist folder

--- src/html2pdf.js
+++ src/html2pdf.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
 const path = require('path');
+// TODO: eslint complains if these are devDependencies, instead of dependencies
 const puppeteer = require('puppeteer');
 const chalk = require('chalk');
+const slugify = require('@sindresorhus/slugify');
 
 // TODO: Handle exceptions
 process.on('unhandledRejection', (up) => { throw up; });
 
 (async () => {
   const htmlPath = path.resolve(process.argv[2]);
-  const pdfPath = `${path.basename(htmlPath, '.html')}.pdf`;
+  const pdfDir = path.dirname(htmlPath);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -17,8 +19,13 @@ process.on('unhandledRejection', (up) => { throw up; });
   console.log();
 
   await page.goto(`file:${htmlPath}`);
-  await page.pdf({ path: `${pdfPath}` });
 
+  const htmlTitle = await page.title();
+  const pdfPath = path.join(pdfDir, `${slugify(htmlTitle)}.pdf`);
+
+  await page.pdf({ path: pdfPath });
+
+  // Styling like pug-cli
   console.log(chalk`  {gray rendered} {cyan ${pdfPath}}`);
 
   await browser.close();
